89982dfadff40f2f98f3162fa950b02c1a8e49c4
Jamie Ly <jamie.ly@gmail.com>
2012-02-27 20:41:45 -0500

    Changed algorithm used to load friends

--- lib/friends_loader.rb
+++ lib/friends_loader.rb
@@ -9,22 +9,36 @@ class FriendsLoader
       graph = Koala::Facebook::API.new(@access_token)
       friends = graph.get_connections("me", "friends").sample(@max_count)
 
-      photos = graph.batch do |batch|
-        friends.each do |friend|
-           batch.get_picture(friend["id"], :type => "large")
+      # Now that we got the friends, sample!
+      assigner = StatisticsAssigner.new friends, @stats
+      assignments = assigner.assign
+
+      # Now reduce the assignments to only those afflicted
+      afflicted_friends = assignments.select do |asmt|
+        !asmt[:statistics].empty?
+      end
+
+      # Get all photos in batches of 49
+      afflicted_friends_photos = []
+      afflicted_friends.each_slice(49) do |friends|
+        # Execute a batch API query
+        photos = graph.batch do |batch|
+          friends.each do |friend|
+             batch.get_picture(friend[:person]["id"], :type => "large")
+          end
         end
+        afflicted_friends_photos.concat(photos)
       end
 
       # Create a hash of friend's names and profile pictures
-      friends_pairs = friends.collect{|friend| friend["name"]}.zip(photos)
-      friends_hash = Hash[friends_pairs]
+      friends_pairs = afflicted_friends.zip(afflicted_friends_photos)
 
-      # Assignments
-      assigner = StatisticsAssigner.new friends_pairs, @stats
-      assignments = assigner.assign
-
-      @friends = assignments.collect do |assignment|
-        Person.new(assignment[:person].first, assignment[:person].last, assignment[:statistics])
+      # Now create Person objects to hold the information
+      @friends = friends_pairs.collect do |pair|
+        friend_info = pair.first
+        photo = pair.last
+        Person.new(friend_info[:person]["name"], 
+                   photo, friend_info[:statistics])
       end
 
       @friends
